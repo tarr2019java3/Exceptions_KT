bbda1b8286836c809b48ac4ea4068c7652020297
Trzaska <euroekspertkrystian@gmail.com>
2020-03-08 10:20:10 +0100

    try with ASCII!

---
+++ src/main/java/pl/sda/wyjatki/User.java
@@ -0,0 +1,27 @@
+package pl.sda.wyjatki;
+
+public class users {
+    private String email;
+    private String login;
+
+    public users(String email, String login) {
+        this.email = email;
+        this.login = login;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+}
